0364d50ea5eb3a09735d99b0ebb5bff6633a519a
mtgalla <mtgalla@gmail.com>
2019-05-08 20:16:32 -0500

    update routes and fix errors

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,37 +1,37 @@
-var db = require("../models");
+// var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+// module.exports = function(app) {
+//   // Get all examples
+//   app.get("/api/examples", function(req, res) {
+//     db.Example.findAll({}).then(function(dbExamples) {
+//       res.json(dbExamples);
+//     });
+//   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+//   // Create a new example
+//   app.post("/api/examples", function(req, res) {
+//     db.Example.create(req.body).then(function(dbExample) {
+//       res.json(dbExample);
+//     });
+//   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-};
+//   // Delete an example by id
+//   app.delete("/api/examples/:id", function(req, res) {
+//     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.json(dbExample);
+//     });
+//   });
+// };
 
-  // Put route for updating an example
-  app.put("/api/examples", function(req, res) {
-    db.Example.update(
-      req.body,
-      {
-        where: {
-          id: req.body.id
-        }
-      }).then(function(dbExample) {
-        res.json(dbExample);
-      });
-  });
+//   // Put route for updating an example
+//   app.put("/api/examples", function(req, res) {
+//     db.Example.update(
+//       req.body,
+//       {
+//         where: {
+//           id: req.body.id
+//         }
+//       }).then(function(dbExample) {
+//         res.json(dbExample);
+//       });
+//   });
