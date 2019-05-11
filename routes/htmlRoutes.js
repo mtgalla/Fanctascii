1920d783b5d917bfc4354baaf89833775ffcbe9c
mtgalla <mtgalla@gmail.com>
2019-05-11 12:47:20 -0500

    update htmlroute name

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
   // });
 
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/testHTML.html"));
+    res.sendFile(path.join(__dirname, "../views/index.html"));
   });
 
   }
